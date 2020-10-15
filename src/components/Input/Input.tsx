841f4827d81e308fedd12d9767a5ee5473a735b4
Kirill Eremin <kirilleremin@yandex-team.ru>
2020-10-16 01:02:00 +0300

    build

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,15 @@
 import React, {FunctionComponent} from 'react';
-// import cn from 'classnames/bind';
+import cn from 'classnames/bind';
 
 import {InputProps} from './Input.types';
 
 import * as styles from './Input.pcss';
 
-// const cx = cn.bind(styles);
+const cx = cn.bind(styles);
 
 export const Input: FunctionComponent<InputProps> = () => {
-    debugger;
-    console.log(styles);
-    //cx('root')
-    return <input className="" type="text" placeholder="components"/>;
+    console.log(styles)
+    return <input className={cx('root')} type="text" placeholder="components"/>;
 };
 
 export default Input;
